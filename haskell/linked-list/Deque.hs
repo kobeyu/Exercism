403eb09b0425cebc3886a72455d4cabbec0d75fe
Greg Manning <git@app.tockli.me>
2014-10-30 14:44:47 +0000

    more simplifying Deque

--- haskell/linked-list/Deque.hs
+++ haskell/linked-list/Deque.hs
@@ -1,30 +1,27 @@
 {-# LANGUAGE RecursiveDo #-}
 module Deque (mkDeque, push, pop, shift, unshift) where
 import Control.Concurrent.STM(atomically,STM)
-import Control.Concurrent.STM.TVar
+import Control.Concurrent.STM.TVar(TVar,newTVar,readTVar,writeTVar)
+import Control.Monad(liftM2)
 
-data Deque a = MkDeque {front,back :: DoubleList a}
-
-data DoubleList a = EndL {next :: TVar (DoubleList a)} 
-                  | EndR {prev :: TVar (DoubleList a)}
-                  | Cons {_this :: a, prev,next :: TVar (DoubleList a)}
+data Deque a = EndLR {next,prev :: TVar (Deque a)} 
+             | Cons {_this :: a, prev,next :: TVar (Deque a)}
 
 mkDeque :: IO (Deque a)
 mkDeque = atomically $ mdo 
-    t1 <- newTVar (EndL t2)
-    t2 <- newTVar (EndR t1)
-    return $ MkDeque (EndL t2) (EndR t1)
+    let m = EndLR t1 t2 
+    t1 <- newTVar m
+    t2 <- newTVar m
+    return m
 
-insert :: a -> DoubleList a -> STM ()
+insert :: a -> Deque a -> STM ()
 insert a nextItem = do
 	prevItem <- readTVar (prev nextItem)
-	prevPtr <- newTVar prevItem
-	nextPtr <- newTVar nextItem
-	let newItem = Cons a prevPtr nextPtr
+	newItem <- liftM2 (Cons a) (newTVar prevItem) (newTVar nextItem)
 	writeTVar (prev nextItem) newItem
 	writeTVar (next prevItem) newItem
 
-delete :: DoubleList a -> STM ()
+delete :: Deque a -> STM ()
 delete t = do
 	prevItem <- readTVar (prev t)
 	nextItem <- readTVar (next t)
@@ -32,18 +29,18 @@ delete t = do
 	writeTVar (next prevItem) nextItem
 
 unshift :: Deque a -> a -> IO ()
-unshift d a = atomically $ readTVar (next (front d)) >>= insert a
+unshift d a = atomically $ readTVar (next d) >>= insert a
 
 push :: Deque a -> a -> IO ()
-push d a = atomically $ insert a (back d)
+push d a = atomically $ insert a d
 
-deleteCons :: DoubleList a -> STM (Maybe a)
+deleteCons :: Deque a -> STM (Maybe a)
 deleteCons l = case l of 
 	Cons t _ _ -> delete l >> return (Just t)
 	_          -> return Nothing
 
 pop :: Deque a -> IO (Maybe a)
-pop d = atomically $ readTVar (prev (back d)) >>= deleteCons
+pop d = atomically $ readTVar (prev d) >>= deleteCons
 
 shift :: Deque a -> IO (Maybe a)
-shift d = atomically $ readTVar (next (front d)) >>= deleteCons
+shift d = atomically $ readTVar (next d) >>= deleteCons
