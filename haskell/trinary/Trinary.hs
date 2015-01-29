368aee7b31b19340c8920de2bffff6bd61f5b3b0
Greg Manning <git@app.tockli.me>
2015-01-29 14:24:09 +0000

    update trinary to how octal is done

--- haskell/trinary/Trinary.hs
+++ haskell/trinary/Trinary.hs
@@ -1,23 +1,19 @@
 {-# LANGUAGE BangPatterns #-}
 module Trinary (showTri, readTri) where
+import Data.List(foldl')
 
 showTri :: (Integral a, Show a) => a -> String
 showTri = concatMap show . digits 3 []
 
 readTri :: (Integral a) => String -> a
-readTri = undigits 3 0 . map readChar
+readTri = undigits 3 . map readChar
 
 digits :: (Integral a) => a -> [a] -> a -> [a]
 digits _ acc 0 = acc
 digits b !acc a = let (d,r) = a `divMod` b in digits b (r:acc) d
 
-undigits :: (Integral a) => a -> a -> [a] -> a
-undigits _ acc [] = acc
-undigits b !acc (a:as) = undigits b (acc * b + a) as
+undigits :: (Integral a) => a -> [a] -> a
+undigits b = foldl' ((+) . (b*)) 0
 
 readChar :: (Integral a) => Char -> a
-readChar c = case c of
-	'0' -> 0
-	'1' -> 1
-	'2' -> 2
-	_ -> error $ "Bad digit: " ++ [c]
+readChar = fromIntegral . subtract (fromEnum '0') . fromEnum
