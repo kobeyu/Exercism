d18bd18989e0243b4cd69c322e1c85ac667f0246
Greg Manning <git@app.tockli.me>
2015-03-12 14:32:18 +0000

    sieve which only marks multiples of primes.

--- haskell/sieve/Sieve.hs
+++ haskell/sieve/Sieve.hs
@@ -1,12 +1,22 @@
 module Sieve (primesUpTo) where
-import qualified Data.Vector as V(elemIndices,create,toList)
-import qualified Data.Vector.Mutable as MV(replicate,write)
+import Control.Monad(filterM)
+import Data.STRef
+import Control.Loop
+import qualified Data.Vector as V(elemIndices,create,toList,findIndex,freeze)
+import qualified Data.Vector.Mutable as MV(replicate,write,slice,read)
 
 primesUpTo ::  Int -> [Int]
 primesUpTo n = V.toList . V.elemIndices True $ V.create $ do
     mv <- MV.replicate (n+1) True
     let clear = mapM_ $ flip (MV.write mv) False
     clear [0..1]
-    mapM_ (\i -> clear [2*i,3*i..n]) [2..n]
+    let markNonPrimesFrom p = do
+        primeCandidates <- filterM (MV.read mv) [p..n]
+        case primeCandidates of
+            [] -> return ()
+            (x:_) -> do
+                clear [2*x,3*x..n]
+                markNonPrimesFrom (x+1)
+    markNonPrimesFrom 2
     return mv
 
