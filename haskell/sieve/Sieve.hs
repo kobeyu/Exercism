4680a0dab60bc7f99535456391efc30545e7b1c5
Greg Manning <git@app.tockli.me>
2015-03-12 16:27:14 +0000

    much tidier Sieve.

--- haskell/sieve/Sieve.hs
+++ haskell/sieve/Sieve.hs
@@ -1,5 +1,5 @@
 module Sieve (primesUpTo) where
-import Control.Monad(filterM)
+import Control.Monad(forM_,when)
 import qualified Data.Vector as V(elemIndices,create,toList)
 import qualified Data.Vector.Mutable as MV(replicate,write,read)
 
@@ -8,13 +8,6 @@ primesUpTo n = V.toList . V.elemIndices True $ V.create $ do
     mv <- MV.replicate (n+1) True
     let clear = mapM_ $ flip (MV.write mv) False
     clear [0..1]
-    let markNonPrimesFrom p = do
-        primeCandidates <- filterM (MV.read mv) [p..n]
-        case primeCandidates of
-            [] -> return ()
-            (x:_) -> do
-                clear [2*x,3*x..n]
-                markNonPrimesFrom (x+1)
-    markNonPrimesFrom 2
+    forM_ [2..n] $ \i -> MV.read mv i >>= flip when (clear [2*i,3*i..n])
     return mv
 
