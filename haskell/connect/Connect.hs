4253b853c9d4bc6b686e1a701cadf97b522b57b3
Greg Manning <git@app.tockli.me>
2015-03-16 14:13:43 +0000

    *a* solution to connect...

--- haskell/connect/Connect.hs
+++ haskell/connect/Connect.hs
@@ -1,29 +1,42 @@
 module Connect(resultFor, Color(Black, White)) where
 import Data.List(transpose)
-import qualified Data.Array as A
+import qualified Data.Array as A(array,bounds,(!),assocs, Array() )
+import qualified Data.Set as S(toList,fromList,Set(),map)
 
 data Color = Black | White deriving (Eq,Show)
 
-data Cell = Empty | Full {left,right :: Bool}
+type CellLoc = (Int,Int)
+type Board = A.Array CellLoc Bool
 
-type Board = A.Array (Int, Int) Cell
-
-mkBoard :: [String] -> Char -> Board
-mkBoard s = A.array ((1,1),(width,height)) assocs
+mkBoard :: Char -> [String] -> Board
+mkBoard c s = A.array ((1,1),(width,height)) assocs
     where
         height = length s
         width = length $ head s
-        assocs = undefined
+        assocs = [((x,y),e == c) | (y,r) <- zip [1..] s, (x,e) <- zip [1..] r]
 
+neighbours :: CellLoc -> (CellLoc,CellLoc) -> [CellLoc]
+neighbours (x,y) ((minX,minY),(maxX,maxY))
+    = [(x',y') | x' <- [x-1,x,x+1],y' <- [y-1,y,y+1]
+                 , x' >= minX, x' <= maxX, y' >= minY, y' <= maxY
+                 , (x',y') `notElem` [(x-1,y-1),(x+1,y+1)]]
 
-isWin :: [[Bool]] -> Bool
-isWin = undefined
+lrConnect :: Board -> Bool
+lrConnect b = f leftCol
+    where
+        leftCol = S.fromList . map fst . filter (\((a,_),e) -> a == 1 && e) . A.assocs $ b
+        ((_,_),(maxX,_)) = A.bounds b
+        f :: S.Set (Int,Int) -> Bool
+        f connected = good || (newConnected /= connected && f newConnected)
+            where
+            good = any ((==maxX) . fst) $ S.toList newConnected
+            newConnected = S.fromList [p | c <- S.toList connected, p <- neighbours c (A.bounds b), b A.! p]
 
 whiteWin :: [String] -> Bool
-whiteWin = isWin . transpose . (map.map) (=='O')
+whiteWin = lrConnect . mkBoard 'O' . transpose
 
 blackWin :: [String] -> Bool
-blackWin = isWin . (map . map) (=='X')
+blackWin = lrConnect . mkBoard 'X'
 
 resultFor :: [String] -> Maybe Color
 resultFor b
